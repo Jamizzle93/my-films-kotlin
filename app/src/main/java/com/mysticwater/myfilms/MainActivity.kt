9a2b72b876600180f408b34aa4cdcb6aeec2b3c2
James Warner <james.warner@raspberrysoftware.com>
2017-08-08 19:52:31 +0100

    feat(MainActivity): Implement initial code for selecting navigation items

--- app/src/main/java/com/mysticwater/myfilms/MainActivity.kt
+++ app/src/main/java/com/mysticwater/myfilms/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.mysticwater.myfilms
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navbar = findViewById<BottomNavigationView>(R.id.nav_bottom)
+        navbar.setOnNavigationItemReselectedListener({ item -> selectFragment(item) })
+    }
+
+    fun selectFragment(item: MenuItem) {
+
+        item.isChecked = true
+
+        when (item.itemId) {
+            R.id.menu_now_showing -> {
+                // TODO - Show fragment
+            }
+            R.id.menu_upcoming -> {
+                // TODO - Show fragment
+            }
+            R.id.menu_favourite -> {
+                // TODO - Show fragment
+            }
+        }
     }
 }
