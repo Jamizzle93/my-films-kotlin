925f23dc878b6f3327e14b98135e6a78c22b17bf
James Warner <james.warner@raspberrysoftware.com>
2017-08-09 19:50:15 +0100

    feat(ActivityUtils): Add utility for adding a fragment

---
+++ app/src/main/java/com/mysticwater/myfilms/util/ActivityUtils.kt
@@ -0,0 +1,16 @@
+package com.mysticwater.myfilms.util
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+
+object ActivityUtils {
+
+
+    fun addFragmentToActivity(fragmentManager: FragmentManager,
+                              fragment: Fragment, frameId: Int) {
+        val transaction = fragmentManager.beginTransaction()
+        transaction.add(frameId, fragment)
+        transaction.commit()
+    }
+
+}
