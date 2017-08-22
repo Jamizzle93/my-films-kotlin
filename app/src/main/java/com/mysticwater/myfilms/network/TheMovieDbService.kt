b8153de7d6e1843fe18ad0b0eb7811f261751ab0
James Warner <jamesnwarner@gmail.com>
2017-08-22 19:50:02 +0100

    feat(*): Add TheMovieDbService and methods to fetch upcoming films

---
+++ app/src/main/java/com/mysticwater/myfilms/network/TheMovieDbService.kt
@@ -0,0 +1,31 @@
+package com.mysticwater.myfilms.network
+
+import com.mysticwater.myfilms.BuildConfig
+import com.mysticwater.myfilms.data.FilmResults
+import io.reactivex.Observable
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface TheMovieDbService {
+
+    @GET("discover/movie?api_key=" + BuildConfig.API_KEY)
+    fun getUpcomingReleases(@Query("region") region: String,
+                            @Query("primary_release_date.gte") startDate: String,
+                            @Query("primary_release_date.lte") endDate: String): Observable<FilmResults>
+
+    companion object {
+        val BASE_URL = "https://api.themoviedb.org/3/"
+
+        fun getTmdbService(): TheMovieDbService {
+            return Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(MoshiConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .build()
+                    .create(TheMovieDbService::class.java)
+        }
+    }
+}
