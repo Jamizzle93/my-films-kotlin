8d0db6a2fa2fd7ce51a24243f00913f61991acc3
James Warner <jamesnwarner@gmail.com>
2018-01-23 20:02:36 +0000

    feat(Film): Make poster_path optional

--- app/src/main/java/com/mysticwater/myfilms/data/Film.kt
+++ app/src/main/java/com/mysticwater/myfilms/data/Film.kt
@@ -7,7 +7,7 @@ import android.arch.persistence.room.Entity
 data class Film constructor(
         @ColumnInfo(name = "id") var id: Int = 0,
         @ColumnInfo(name = "title") var title: String = "",
-        @ColumnInfo(name = "poster_path") var poster_path: String = "",
+        @ColumnInfo(name = "poster_path") var poster_path: String? = "",
         @ColumnInfo(name = "release_date") var release_date: String = "") : Comparable<Film> {
 
 
